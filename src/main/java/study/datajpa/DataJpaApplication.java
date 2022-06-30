e694cb8f040eb0fca6e37f9b055627953353a4bd
LCH <ckcks1997@gmail.com>
2022-06-30 21:14:26 +0900

    스프링 데이터 JPA - 공통인터페이스 기능
    -JpaRepository 인터페이스 상속을 이용한 Repository구현 실습

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -1,7 +1,15 @@
 package study.datajpa;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 
 @SpringBootApplication
 public class DataJpaApplication {
