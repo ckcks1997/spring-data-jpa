44606ed051613f2c94874de087113acff15f3179
LCH <ckcks1997@gmail.com>
2022-07-03 15:08:13 +0900

    스프링 데이터 JPA - 확장기능
    - 사용자 리포지토리, Auditing

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -3,6 +3,9 @@ package study.datajpa;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.io.*;
@@ -10,7 +13,10 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
+import java.util.Optional;
+import java.util.UUID;
 
+@EnableJpaAuditing//auditing 사용하려면 넣어야됨
 @SpringBootApplication
 public class DataJpaApplication {
 
@@ -18,4 +24,13 @@ public class DataJpaApplication {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
 
+	@Bean //실무에서는 스프링 시큐리티등을 이용해 세션의 아이디값을 넣는다.
+	public AuditorAware<String> auditorProvider(){
+		return new AuditorAware<String>() {
+			@Override
+			public Optional<String> getCurrentAuditor() {
+				return Optional.of(UUID.randomUUID().toString());
+			}
+		};
+	}
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -199,4 +199,26 @@ class MemberRepositoryTest {
         }
     }
 
+    @Test
+    public void findCustom(){
+        List<Member> memberCustom = memberRepository.findMemberCustom();
+    }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member); //@PrePersist
+        Thread.sleep(100);
+        member.setUsername("member2");
+        em.flush(); //@PreUpdate
+        em.clear();
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+        //then
+        System.out.println("findMember.createdDate = " +
+                findMember.getCreatedDate());
+        System.out.println("findMember.updatedDate = " +
+                findMember.getLastModifiedBy());
+    }
 }
